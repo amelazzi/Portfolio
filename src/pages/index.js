00d771bb766d7af77e39c0cd2c26273bb85f5af3
amel azzi <amelazzi@amels-MacBook-Pro.local>
2022-10-01 17:27:45 +0200

    :fire: Remove experience section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,6 @@ import SEO from "../components/seo"
 
 import HomeSection from '../components/home/section'
 import SkillsSection from '../components/skills/section'
-import ExperiencesSection from '../components/experiences/section'
 import MyProjects from '../components/projects/section'
 import EventsSection from '../components/events/section'
 import ContactSection from '../components/contact/section'
@@ -15,7 +14,6 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
         <HomeSection/>
-        <ExperiencesSection/>
         <SkillsSection/>
         <MyProjects/>
         <EventsSection/>
